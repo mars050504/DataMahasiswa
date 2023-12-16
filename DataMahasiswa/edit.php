fd0bc09b2c38704adee32cda69f0e4eb4314d00e
mars050504 <153522723+mars050504@users.noreply.github.com>
2023-12-16 21:07:58 +0700

    Update edit.php

--- DataMahasiswa/edit.php
+++ DataMahasiswa/edit.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["id"])) {
     $id = $_GET["id"];
 
     // Ambil data mahasiswa berdasarkan ID
-    $result = $koneksi->query("SELECT * FROM tb_mahasiswa WHERE id_mahasiswa = '$id'");
+    $result = $koneksi->query("SELECT * FROM data_mhs WHERE id_mahasiswa = '$id'");
 
     if ($result->num_rows == 1) {
         $data = $result->fetch_assoc();
@@ -15,10 +15,16 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["id"])) {
                 <input type='hidden' name='id' value='" . $data["id_mahasiswa"] . "'>
                 <label for='name'>Nama:</label>
                 <input type='text' id='name' name='name' value='" . $data["Nama"] . "' required>
-                <label for='angkatan'>Angkatan:</label>
-                <input type='text' id='angkatan' name='angkatan' value='" . $data["Angkatan"] . "' required>
-                <label for='NPM'>NPM:</label>
-                <input type='number' id='NPM' name='NPM' value='" . $data["NPM"] . "' required>
+                <label for='Tahun_Masuk'>Tahun_Masuk:</label>
+                <input type='text' id='Tahun_Masuk' name='Tahun_Masuk' value='" . $data["Tahun_Masuk"] . "' required>
+                <label for='Prodi'>Prodi:</label>
+                <input type='text' id='Prodi' name='Prodi' value='" . $data["Prodi"] . "' required>
+                <label for='UKT'>UKT:</label>
+                <input type='number' id='UKT' name='UKT' value='" . $data["UKT"] . "' required>
+                <label for='Alamat'>Alamat:</label>
+                <input type='text' id='Alamat' name='Alamat' value='" . $data["Alamat"] . "' required>
+                <label for='Email'>Email:</label>
+                <input type='text' id='Email' name='Email' value='" . $data["Email"] . "' required>
                 <button type='submit' name='submit_edit'>Simpan Perubahan</button>
             </form>
         ";
@@ -27,4 +33,3 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["id"])) {
     }
 }
 ?>
-
