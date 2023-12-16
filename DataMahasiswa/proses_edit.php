a60b44c049f2224263938d78f11f4428e0633dbe
mars050504 <153522723+mars050504@users.noreply.github.com>
2023-12-16 21:07:44 +0700

    Update proses_edit.php

--- DataMahasiswa/proses_edit.php
+++ DataMahasiswa/proses_edit.php
@@ -4,11 +4,14 @@ include("koneksi.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit_edit"])) {
     $id = $_POST["id"];
     $name = $_POST["name"];
-    $angkatan = $_POST["angkatan"];
-    $NPM = $_POST["NPM"];
+    $Tahun_Masuk = $_POST["Tahun_Masuk"];
+    $Prodi = $_POST["Prodi"];
+    $UKT = $_POST["UKT"];
+    $Alamat = $_POST["Alamat"];
+    $Email = $_POST["Email"];
 
     // Lakukan update data di tabel
-    $sql = "UPDATE tb_mahasiswa SET Nama='$name', Angkatan='$angkatan', NPM='$NPM' WHERE id_mahasiswa='$id'";
+    $sql = "UPDATE data_mhs SET Nama='$name', Tahun_Masuk='$Tahun_Masuk', Prodi='$Prodi', UKT='$UKT', Alamat='$Alamat', Email='$Email' WHERE id_mahasiswa='$id'";
 
     if ($koneksi->query($sql) === TRUE) {
         echo "Data berhasil diupdate.";
